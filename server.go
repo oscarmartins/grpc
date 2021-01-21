7753d0af859e8ef11ba51e34d7ffea5985c6a2f3
Oscar Rafael <oscarrafaelcampos@gmail.com>
2021-01-21 23:40:03 +0000

    chat v1

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net"
 
+	"github.com/oscarmartins/grpc/chat"
 	"google.golang.org/grpc"
 )
 
@@ -13,7 +14,11 @@ func main() {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 
+	s := chat.Server{}
+
 	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve grpc server over port 9000 %v", err)
