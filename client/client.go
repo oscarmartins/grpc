3592153b5888b5b34cabcd57a09242dfec200048
Oscar Rafael <oscarrafaelcampos@gmail.com>
2021-01-22 00:16:06 +0000

    fix

--- client/client.go
+++ client/client.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	conn, err := grpc.Dial(":9080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("could not connect: %s", err)
 	}
--- server.go
+++ server.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9000")
+	lis, err := net.Listen("tcp", ":9080")
 	if err != nil {
-		log.Fatalf("Failed to listen on port 9000: %v", err)
+		log.Fatalf("Failed to listen on port 9080: %v", err)
 	}
 
 	s := chat.Server{}
